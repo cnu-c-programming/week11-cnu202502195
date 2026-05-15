ae2bc83f7a96c4d7cf40c04e56b40dc17d782372
cnu202502195 <202502195@o.cnu.ac.kr>
2026-05-15 09:18:58 +0900

    2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+enum Color { RED, GREEN, BULE };
+
+enum Status { OK = 0, ERR = -1, BUSY = 1};
+
+int main(void) {
+    printf("RED   = %d\n", RED);
+    printf("GREEN = %d\n", GREEN);
+    printf("BULE  = %d\n\n", BULE);
+
+    printf("OK    = %d\n", OK);
+    printf("ERR   = %d\n", ERR);
+    printf("BUSY  = %d\n", BUSY);
+
+    return 0;
+}
