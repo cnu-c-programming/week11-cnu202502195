722d92b339aeb4002c3cd2f0d4fc384aca53673f
cnu202502195 <202502195@o.cnu.ac.kr>
2026-05-15 10:01:39 +0900

    3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -8,7 +8,7 @@ int mul(int a, int b) { return a * b; }
 
 int compute(int a, int b, BinOp op) {
     if(op == NULL) {
-        printf("Null pointer");
+        printf("Null pointer\n");
         return -1;
     }
     return op(a, b);
